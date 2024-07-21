1a19dff2ca6413405c998ada2a33ef95586277b7
Daniel <Daniel@DESKTOP-R2FSIL9>
2024-07-20 23:34:57 -0600

    MODULO 2

---
+++ src/principal/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Recursos recursos = new Recursos(); 
+
+        System.out.println("Resultado del metodo numero uno: " + recursos.Mensaje());
+
+        int edadEstudiante = 22; 
+        System.out.println("Resultado del método numero dos: " + recursos.Edad(edadEstudiante));
+
+        int num1 = 10;
+        int num2 = 4;
+        System.out.println("Resultado del método numero tres: " + recursos.Multiplicacion(num1, num2));
+        
+        int x = 15;
+        System.out.println("Resultado del cuarto metodo:");
+        System.out.println(recursos.NumeroX(x));
+       
+        
+    }
+}
